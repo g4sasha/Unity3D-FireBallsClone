59976c973c0f8673025cc67fd6977a2ecb4fefae
Александр Гяч <as.gyach@gmail.com>
2024-12-11 06:27:32 +0300

    refactor: added scriptableObjects for towers and guns

--- Assets/_Source/Core/Bootstrap.cs
+++ Assets/_Source/Core/Bootstrap.cs
@@ -1,6 +1,6 @@
-using System.Collections.Generic;
 using Components;
 using ObjectPool;
+using ScriptableObjects;
 using UnityEngine;
 
 namespace Core
@@ -9,21 +9,23 @@ namespace Core
     {
         [Header("Gun")]
         [SerializeField] private Gun _gun;
-        [SerializeField] private Bullet _bulletPrefab;
         [SerializeField] private Transform _bulletStorage;
-        [SerializeField] private int _bulletPoolSize;
+        [SerializeField] private GunSO _gunConfiguration;
 
         [Header("Tower")]
         [SerializeField] private Tower _tower;
-        [SerializeField] private int _towerSize;
-        [SerializeField] private List<GameObject> _towerParts;
-        
+        [SerializeField] private TowerSO _towerConfiguration;
+
         private BulletPool _bulletPool;
 
         private void Start()
         {
-            _tower.Generate(_towerSize, _towerParts);
-            _bulletPool = new BulletPool(_bulletPrefab, _bulletPoolSize, _bulletStorage);
+            var bulletPrefab = _gunConfiguration.BulletPrefab;
+            var initBulletPoolSize = _gunConfiguration.BulletInitPoolSize;
+
+            _tower.Generate(_towerConfiguration.Size, _towerConfiguration.Parts);
+            
+            _bulletPool = new BulletPool(bulletPrefab, initBulletPoolSize, _bulletStorage);
             _gun.Construct(_bulletPool);
         }
     }
