e4e4f887727d2630f0cccf02f64bc80cc1e4f39e
Александр Гяч <as.gyach@gmail.com>
2024-12-11 06:11:50 +0300

    refactor: small fixes

--- Assets/_Source/Components/Tower.cs
+++ Assets/_Source/Components/Tower.cs
@@ -1,57 +1,59 @@
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace Components
-
 {
     public class Tower : MonoBehaviour
     {
-        [SerializeField] private List<GameObject> _parts;
-        [SerializeField] private List<GameObject> _partsTypes;
+        [SerializeField] private List<GameObject> _activeParts;
 
-        private void Start()
+#if UNITY_EDITOR
+        private void OnValidate()
         {
-            Generate(100, _partsTypes);
+            _activeParts = GetComponentsInChildren<Transform>().Select(t => t.gameObject).ToList();
+            _activeParts.RemoveAt(0);
         }
+#endif
 
         private void OnTriggerEnter(Collider other)
         {
             if (other.gameObject.tag == "Bullet")
             {
-                Destroy(_parts[0]);
-                _parts.RemoveAt(0);
+                Destroy(_activeParts[0]);
+                _activeParts.RemoveAt(0);
                 other.gameObject.SetActive(false);
                 MoveParts();
             }
 
-            if (_parts.Count.Equals(0))
+            if (_activeParts.Count.Equals(0))
             {
                 Destroy(gameObject);
             }
         }
 
-        public void Generate(int count, List<GameObject> parts)
+        public void Generate(int count, List<GameObject> partPrefabs)
         {
-            foreach (var part in _parts)
+            foreach (var part in _activeParts)
             {
                 Destroy(part);
             }
 
-            _parts = new List<GameObject>();
+            _activeParts.Clear();
 
             for (int i = 0; i < count; i++)
             {
-                var nextPart = parts[i % parts.Count];
-                Debug.Log("NextPart: " + nextPart);
-                var position = transform.position + new Vector3(0f, i * nextPart.transform.localScale.y * 2f + nextPart.transform.localScale.y, 0f);
+                var nextPart = partPrefabs[i % partPrefabs.Count];
+                var moveY = i * nextPart.transform.localScale.y * 2f + nextPart.transform.localScale.y;
+                var position = transform.position + new Vector3(0f, moveY, 0f);
                 var part = Instantiate(nextPart, position, Quaternion.identity, transform);
-                _parts.Add(part);
+                _activeParts.Add(part);
             }
         }
 
         private void MoveParts()
         {
-            foreach (var part in _parts)
+            foreach (var part in _activeParts)
             {
                 part.transform.Translate(Vector3.down * part.transform.localScale.y * 2f);
             }
--- Assets/_Source/Core/Bootstrap.cs
+++ Assets/_Source/Core/Bootstrap.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Components;
 using ObjectPool;
 using UnityEngine;
@@ -6,14 +7,22 @@ namespace Core
 {
     public class Bootstrap : MonoBehaviour
     {
+        [Header("Gun")]
         [SerializeField] private Gun _gun;
         [SerializeField] private Bullet _bulletPrefab;
         [SerializeField] private Transform _bulletStorage;
         [SerializeField] private int _bulletPoolSize;
+
+        [Header("Tower")]
+        [SerializeField] private Tower _tower;
+        [SerializeField] private int _towerSize;
+        [SerializeField] private List<GameObject> _towerParts;
+        
         private BulletPool _bulletPool;
 
         private void Start()
         {
+            _tower.Generate(_towerSize, _towerParts);
             _bulletPool = new BulletPool(_bulletPrefab, _bulletPoolSize, _bulletStorage);
             _gun.Construct(_bulletPool);
         }
